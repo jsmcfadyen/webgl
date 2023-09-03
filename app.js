4b000a1153cbc24bf8a98b186353b3d376bcb544
Jacob McFadyen <jsmcfadyen@gmail.com>
2023-09-03 16:19:35 -0400

    init: spinning cube with user interaction

---
+++ app.js
@@ -0,0 +1,240 @@
+const { mat4 } = glMatrix;
+var vertextShaderText = [
+  "precision mediump float;",
+  "",
+  "attribute vec3 vertPosition;",
+  "attribute vec3 vertColor;",
+  "varying vec3 fragColor;",
+  "uniform mat4 mWorld;",
+  "uniform mat4 mView;",
+  "uniform mat4 mProj;",
+  "",
+  "void main()",
+  "{",
+  "   fragColor = vertColor;",
+  "   gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);",
+  "}",
+].join("\n");
+
+var fragmentShaderText = [
+  "precision mediump float;",
+  "",
+  "varying vec3 fragColor;",
+  "void main()",
+  "{",
+  "   gl_FragColor = vec4(fragColor, 1.0);",
+  "}",
+].join("\n");
+
+
+
+const InitDemo = () => {
+  console.log("This is working");
+  var canvas = document.getElementById("surface");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    console.log("Your browser is using the experimental version of WebGL");
+    gl = canvas.getContext("experimental-webgl");
+  }
+  if (!gl) alert("Your browser does not support WebGL");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+
+  // USER INPUT
+  // -1 <= (x, y) <= 1
+  let userInputXYValue = {
+    x: 0,
+    y: 0,
+  }
+  const handleMouseMove = (e) => {
+    userInputXYValue.x = e.clientX / canvas.width
+    userInputXYValue.y = e.clientY / canvas.height 
+  }
+  canvas.addEventListener('mousemove', 
+    handleMouseMove
+  ) 
+
+
+  gl.clearColor(0.75, 0.85, 0.8, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT, gl | gl.DEPTH_BUFFER_BIT);
+  gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.CULL_FACE);
+  gl.frontFace(gl.CCW);
+  gl.cullFace(gl.BACK);
+
+  // shader pipeline
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertexShader, vertextShaderText);
+  gl.shaderSource(fragmentShader, fragmentShaderText);
+
+  gl.compileShader(vertexShader);
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error(
+      "ERROR compiling vertex shader",
+      gl.getShaderInfoLog(vertexShader)
+    );
+    return;
+  }
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error(
+      "ERROR compiling fragment shader",
+      gl.getShaderInfoLog(fragmentShader)
+    );
+    return;
+  }
+
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error("ERROR linking program", gl.getProgramInfoLog(program));
+    return;
+  }
+
+  // optional step
+  gl.validateProgram(program);
+  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+    console.error("ERROR validating program", gl.getProgramInfoLog(program));
+    return;
+  }
+
+  // create vertex buffer
+  var boxVerticies = [
+    // X, Y, Z           R, G, B
+    // Top
+    -1.0, 1.0, -1.0, 0.5, 0.5, 0.5, -1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 1.0, 1.0,
+    1.0, 0.5, 0.5, 0.5, 1.0, 1.0, -1.0, 0.5, 0.5, 0.5,
+
+    // Left
+    -1.0, 1.0, 1.0, 0.75, 0.25, 0.5, -1.0, -1.0, 1.0, 0.75, 0.25, 0.5, -1.0,
+    -1.0, -1.0, 0.75, 0.25, 0.5, -1.0, 1.0, -1.0, 0.75, 0.25, 0.5,
+
+    // Right
+    1.0, 1.0, 1.0, 0.25, 0.25, 0.75, 1.0, -1.0, 1.0, 0.25, 0.25, 0.75, 1.0,
+    -1.0, -1.0, 0.25, 0.25, 0.75, 1.0, 1.0, -1.0, 0.25, 0.25, 0.75,
+
+    // Front
+    1.0, 1.0, 1.0, 1.0, 0.0, 0.15, 1.0, -1.0, 1.0, 1.0, 0.0, 0.15, -1.0, -1.0,
+    1.0, 1.0, 0.0, 0.15, -1.0, 1.0, 1.0, 1.0, 0.0, 0.15,
+
+    // Back
+    1.0, 1.0, -1.0, 0.0, 1.0, 0.15, 1.0, -1.0, -1.0, 0.0, 1.0, 0.15, -1.0, -1.0,
+    -1.0, 0.0, 1.0, 0.15, -1.0, 1.0, -1.0, 0.0, 1.0, 0.15,
+
+    // Bottom
+    -1.0, -1.0, -1.0, 0.5, 0.5, 1.0, -1.0, -1.0, 1.0, 0.5, 0.5, 1.0, 1.0, -1.0,
+    1.0, 0.5, 0.5, 1.0, 1.0, -1.0, -1.0, 0.5, 0.5, 1.0,
+  ];
+
+  var boxIndicies = [
+    // Top
+    0, 1, 2, 0, 2, 3,
+
+    // Left
+    5, 4, 6, 6, 4, 7,
+
+    // Right
+    8, 9, 10, 8, 10, 11,
+
+    // Front
+    13, 12, 14, 15, 14, 12,
+
+    // Back
+    16, 17, 18, 16, 18, 19,
+
+    // Bottom
+    21, 20, 22, 22, 20, 23,
+  ];
+
+  var boxVertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(boxVerticies),
+    gl.STATIC_DRAW
+  );
+
+  var boxIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBuffer);
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(boxIndicies),
+    gl.STATIC_DRAW
+  );
+
+  var positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
+  var colorAttribLocation = gl.getAttribLocation(program, "vertColor");
+  gl.vertexAttribPointer(
+    positionAttribLocation,
+    3,
+    gl.FLOAT,
+    gl.FALSE,
+    6 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+  gl.vertexAttribPointer(
+    colorAttribLocation,
+    3,
+    gl.FLOAT,
+    gl.FALSE,
+    6 * Float32Array.BYTES_PER_ELEMENT,
+    3 * Float32Array.BYTES_PER_ELEMENT
+  );
+
+  gl.enableVertexAttribArray(positionAttribLocation);
+  gl.enableVertexAttribArray(colorAttribLocation);
+
+  gl.useProgram(program);
+
+  var matWorldUniformLocation = gl.getUniformLocation(program, "mWorld");
+  var matViewUniformLocation = gl.getUniformLocation(program, "mView");
+  var matProjUniformLocation = gl.getUniformLocation(program, "mProj");
+
+  var worldMatrix = new Float32Array(16);
+  var projMatrix = new Float32Array(16);
+  var viewMatrix = new Float32Array(16);
+
+  mat4.identity(worldMatrix);
+  mat4.lookAt(viewMatrix, [0, 0, -7], [0, 0, 0], [0, 1, 0]);
+  mat4.perspective(
+    projMatrix,
+    Math.PI / 4,
+    canvas.width / canvas.height,
+    0.1,
+    1000.0
+  );
+
+  gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+  gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+  gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+  var identityMatrix = new Float32Array(16);
+  mat4.identity(identityMatrix);
+
+  var yRotationMatrix = new Float32Array(16);
+  var xRotationMatrix = new Float32Array(16);
+  // main loop
+  gl.useProgram(program);
+
+  var angle = 0;
+  const loop = () => {
+    angle = (performance.now() / 1000 / 6) * 2 * Math.PI;
+    mat4.rotate(yRotationMatrix, identityMatrix, angle * userInputXYValue.x, [0, 1, 0]);
+    mat4.rotate(xRotationMatrix, identityMatrix, angle * userInputXYValue.y, [1, 0, 0]);
+    mat4.mul(worldMatrix, xRotationMatrix, yRotationMatrix);
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+    gl.drawElements(gl.TRIANGLES, boxIndicies.length, gl.UNSIGNED_SHORT, 0);
+
+    requestAnimationFrame(loop);
+  };
+  requestAnimationFrame(loop);
+};
